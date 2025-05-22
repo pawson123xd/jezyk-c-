783dcd94b057aa007a3228f7ba8a09d06cd790a5
pawson123xd <115405815+pawson123xd@users.noreply.github.com>
2025-05-22 19:49:20 +0200

    Add files via upload

---
+++ sieć neuronowa/aplikacja oknowa/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            wyswietl = new TextBox();
+            inf = new Label();
+            licznik = new NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)licznik).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(591, 26);
+            button1.Name = "button1";
+            button1.Size = new Size(186, 103);
+            button1.TabIndex = 0;
+            button1.Text = "Zadanie1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(591, 166);
+            button2.Name = "button2";
+            button2.Size = new Size(186, 103);
+            button2.TabIndex = 1;
+            button2.Text = "Zadanie2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(591, 292);
+            button3.Name = "button3";
+            button3.Size = new Size(186, 103);
+            button3.TabIndex = 2;
+            button3.Text = "Zadanie3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(544, 26);
+            label1.Name = "label1";
+            label1.Size = new Size(41, 75);
+            label1.TabIndex = 3;
+            label1.Text = "W  Wy\r\n0 0  0\r\n0 1  1\r\n1 0  1\r\n1 1  0\r\n";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(544, 166);
+            label2.Name = "label2";
+            label2.Size = new Size(44, 75);
+            label2.TabIndex = 4;
+            label2.Text = "We Wy\r\n0 0  0 1\r\n0 1  1 0\r\n1 0  1 0\r\n1 1  0 0\r\n";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(532, 292);
+            label3.Name = "label3";
+            label3.Size = new Size(53, 135);
+            label3.TabIndex = 5;
+            label3.Text = "We    Wy\r\n0 0 0  0 0\r\n0 1 0  1 0\r\n1 0 0  1 0\r\n1 1 0  0 1\r\n0 0 1  1 0\r\n0 1 1  0 1\r\n1 0 1  0 1\r\n1 1 1  1 1\r\n";
+            label3.Click += label3_Click;
+            // 
+            // wyswietl
+            // 
+            wyswietl.BackColor = SystemColors.Window;
+            wyswietl.ForeColor = SystemColors.ActiveCaptionText;
+            wyswietl.Location = new Point(12, 12);
+            wyswietl.Multiline = true;
+            wyswietl.Name = "wyswietl";
+            wyswietl.ReadOnly = true;
+            wyswietl.Size = new Size(182, 426);
+            wyswietl.TabIndex = 6;
+            wyswietl.TextChanged += textBox1_TextChanged;
+            // 
+            // inf
+            // 
+            inf.AutoSize = true;
+            inf.Location = new Point(364, 8);
+            inf.Name = "inf";
+            inf.Size = new Size(110, 15);
+            inf.TabIndex = 8;
+            inf.Text = "podaj liczbe warstw";
+            inf.Click += label4_Click;
+            // 
+            // licznik
+            // 
+            licznik.Location = new Point(364, 24);
+            licznik.Name = "licznik";
+            licznik.Size = new Size(119, 23);
+            licznik.TabIndex = 9;
+            licznik.ValueChanged += numericUpDown1_ValueChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(licznik);
+            Controls.Add(inf);
+            Controls.Add(wyswietl);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)licznik).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        public TextBox wyswietl;
+        private Label inf;
+        private NumericUpDown licznik;
+    }
+}
---
+++ sieć neuronowa/aplikacja oknowa/Form1.cs
@@ -0,0 +1,326 @@
+using System.Windows.Forms;
+using WinFormsApp2;                // zawiera System.Windows.Forms.Application
+namespace WinFormsApp2
+{
+
+    public partial class Form1 : Form
+    {
+        public int wagi_miejsce = 0;
+        public int bias_miejsce = 0;
+        private bool enterPressed = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;            // wa¿ne, ¿eby form przekazywa³ KeyDown
+            this.KeyDown += Form1_KeyDown;
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                enterPressed = true;
+                e.Handled = true;   // opcjonalnie: zapobiega “przepuszczeniu” klawisza dalej
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[,] wejscia = { { 0, 0 }, { 0, 1 }, { 1, 0 }, { 1, 1 } };
+            int[,] wyjscie = { { 0 }, { 1 }, { 1 }, { 0 } };
+            if (licznik.Value <= 1)
+            {
+                MessageBox.Show("najnimum 2 warstwy");
+            }
+            else
+            {
+                wyswietl.Text += "gotowe ";
+                List<double[]> warstwy = new List<double[]>();
+                warstwy = wycztaj1(wejscia, (int)licznik.Value);
+                sieæ Siec = new sieæ(wagi_miejsce, bias_miejsce, warstwy);
+                wyswietl.Text ="";
+                List<double> error1 = new List<double>();
+                error1=Siec.tranig(100,0.3,wejscia, wyjscie);
+                for (int i = 0; i < error1.Count; i++) {
+                    wyswietl.Text +="   ,"+error1[i];
+                }
+
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+        public void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int[,] wejscia = { { 0, 0 }, { 0, 1 }, { 1, 0 }, { 1, 1 } };
+            int[,] wyjscie = { { 0, 1 }, { 1, 0 }, { 1, 0 }, { 0, 0 } };
+            if (licznik.Value <= 1)
+            {
+                MessageBox.Show("najnimum 2 warstwy");
+            }
+            else
+            {
+                wyswietl.Text += "gotowe ";
+                List<double[]> warstwy = new List<double[]>();
+                warstwy = wycztaj1(wejscia, (int)licznik.Value);
+                sieæ Siec = new sieæ(wagi_miejsce, bias_miejsce, warstwy);
+                wyswietl.Text = "";
+                List<double> error1 = new List<double>();
+                error1 = Siec.tranig(100, 0.3, wejscia, wyjscie);
+                for (int i = 0; i < error1.Count; i++)
+                {
+                    wyswietl.Text += "   ," + error1[i];
+                }
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int[,] wejscia = { { 0, 0, 0 }, { 0, 1, 0 }, { 1, 0, 0 }, { 1, 1, 0 }, { 0, 0, 1 }, { 0, 1, 1 }, { 1, 0, 1 }, { 1, 1, 1 } };
+            int[,] wyjscie = { { 0, 0 }, { 1, 0 }, { 1, 0 }, { 0, 1 }, { 1, 0 }, { 0, 1 }, { 0, 1 }, { 1, 1 } };
+            if (licznik.Value <= 1)
+            {
+                MessageBox.Show("najnimum 2 warstwy");
+            }
+            else
+            {
+                wyswietl.Text += "gotowe ";
+                List<double[]> warstwy = new List<double[]>();
+                warstwy = wycztaj1(wejscia, (int)licznik.Value);
+                sieæ Siec = new sieæ(wagi_miejsce, bias_miejsce, warstwy);
+                wyswietl.Text = "";
+                List<double> error1 = new List<double>();
+                error1 = Siec.tranig(100, 0.3, wejscia, wyjscie);
+                for (int i = 0; i < error1.Count; i++)
+                {
+                    wyswietl.Text += "   ," + error1[i];
+                }
+
+            }
+        }
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+        private List<double[]> wycztaj1(int[,] wejscia, int ile_warst)
+        {
+
+            List<double[]> warstwy = new List<double[]>();
+            for (int i = 0; i < ile_warst; i++)
+            {
+                if (i == ile_warst - 1)
+                {
+                    inf.Text = "Podaj ile neuronuw w warstwie wyjsciowy";
+                }
+                else
+                {
+                    inf.Text = "Podaj ile neuronuw w warstwie " + (i + 1);
+                }
+                enterPressed = false;
+
+                // czekamy, a¿ u¿ytkownik wciœnie Enter
+                while (!enterPressed)
+                {
+                    Application.DoEvents();
+                    System.Threading.Thread.Sleep(10);  // odrobina pauzy, by nie “zapchaæ” CPU
+                }
+                double[] neuronuw = new double[(int)licznik.Value];
+                warstwy.Add(neuronuw);
+                if (i == 0)
+                {
+                    wagi_miejsce += wejscia.GetLength(1) * (int)licznik.Value;
+                }
+                else
+                {
+                    wagi_miejsce += warstwy[i - 1].Length * (int)licznik.Value;
+                }
+                bias_miejsce += (int)licznik.Value;
+                }
+            inf.Text = "koniec";
+            return warstwy;
+        }
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
+class sieæ :Form1
+{
+    List<double[]> warstwy = new List<double[]>();
+    public double[] wagi;
+    public double[] bias;
+    public sieæ(int wagi_miejsce, int bias_miejsce, List<double[]> warstwy) 
+    {
+        Random rand = new Random();
+        wagi = new double[wagi_miejsce];
+        bias = new double[bias_miejsce];
+        for (int i = 0; i < wagi_miejsce; i++)
+        {
+            wagi[i] = rand.NextDouble() * 10 - 5;
+        }
+        for (int i = 0; i < bias_miejsce; i++)
+        {
+            bias[i] = rand.NextDouble() * 10 - 5;
+        }
+        this.warstwy = warstwy;
+        for (int i = 0; i < warstwy.Count; i++)
+        {
+            for (int j = 0; j < warstwy[i].Length; j++)
+            {
+                warstwy[i][j] = 0.0;
+            }
+        }
+    }
+    private void propagacja(ref int wagi_miejsce, ref int bias_miejsce, int[,] wejscia, int k)
+    {
+        for (int i = 0; i < warstwy.Count; i++)
+        {
+            for (int j = 0; j < warstwy[i].Length; j++)
+            {
+                if (i == 0)
+                {
+                    for (int l = 0; l < wejscia.GetLength(1); l++)
+                    {
+                        warstwy[i][j] += wejscia[k, l] * wagi[wagi_miejsce];
+                        wagi_miejsce++;
+                    }
+                }
+                else
+                {
+                    for (int l = 0; l < warstwy[i - 1].Length; l++)
+                    {
+                        warstwy[i][j] += warstwy[i - 1][l] * wagi[wagi_miejsce];
+                        wagi_miejsce++;
+                    }
+                }
+                warstwy[i][j] += bias[bias_miejsce];
+                warstwy[i][j] = sigmain(warstwy[i][j]);
+                bias_miejsce++;
+            }
+        }
+        bias_miejsce--;
+        wagi_miejsce--;
+
+    }
+    private void propagacja_wsteczna(ref double[] error, double u, int[,] wejscia, int[,] wyjscie, ref int wagi_miejsce, ref int bias_miejsce, int i)
+    {
+        List<double> kopia_bias = new List<double>();
+        List<double> kopia_wagi = new List<double>();
+        for (int j = 0; j < bias.Length; j++)
+        {
+            kopia_bias.Add(bias[j]);
+        }
+        for (int j = 0; j < wagi.Length; j++)
+        {
+            kopia_wagi.Add(wagi[j]);
+        }
+        for (int j = warstwy[warstwy.Count - 1].Length - 1; j >= 0; j--)
+        {
+            double blad = wyjscie[i, j] - warstwy[warstwy.Count - 1][j];
+            error[j] += Math.Pow(blad, 2);
+            blad = blad * u;
+            warstwy[warstwy.Count - 1][j] = pochodnasigmain(warstwy[warstwy.Count - 1][j], blad);
+            kopia_bias[bias_miejsce] += warstwy[warstwy.Count - 1][j];
+            bias_miejsce--;
+        }
+        for (int j = warstwy.Count - 2; j >= 0; j--)
+        {
+            int licznik = 0;
+            for (int k = warstwy[j].Length - 1; k >= 0; k--)
+            {
+                double suma = 0;
+                int kopia_wagi1 = wagi_miejsce;
+                for (int l = warstwy[j + 1].Length - 1; l >= 0; l--)
+                {
+                    kopia_wagi[kopia_wagi1] += warstwy[j + 1][l] * warstwy[j][k];
+                    kopia_wagi1 -= warstwy[j].Length;
+                }
+                kopia_wagi1 = wagi_miejsce;
+                for (int l = warstwy[j + 1].Length - 1; l >= 0; l--)
+                {
+                    suma += warstwy[j + 1][l] * wagi[kopia_wagi1];
+                    kopia_wagi1 -= warstwy[j].Length;
+
+                }
+                warstwy[j][k] = pochodnasigmain(warstwy[j][k], suma);
+                kopia_bias[bias_miejsce] -= warstwy[j][k];
+                bias_miejsce--;
+                if (k == 0)
+                {
+                    wagi_miejsce += licznik;
+                    wagi_miejsce -= warstwy[j].Length * warstwy[j + 1].Length;
+                }
+                else
+                {
+                    wagi_miejsce--;
+                    licznik++;
+                }
+            }
+        }
+        for (int j = warstwy[0].Length - 1; j >= 0; j--)
+        {
+            for (int k1 = wejscia.GetLength(1) - 1; k1 >= 0; k1--)
+            {
+                kopia_wagi[wagi_miejsce] += wejscia[i, k1] * warstwy[0][j];
+                wagi_miejsce--;
+            }
+        }
+        for (int j = 0; j < wagi.Length; j++)
+        {
+            wagi[j] = kopia_wagi[j];
+        }
+        for (int j = 0; j < bias.Length; j++)
+        {
+            bias[j] = kopia_bias[j];
+        }
+        wagi_miejsce = 0;
+        bias_miejsce = 0;
+    }
+    public List<double> tranig(int epok, double u, int[,] wejscia, int[,] wyjscie)
+    {
+        List<double> error1 =new List<double>();
+        for (int j = 0; j < epok; j++)
+        {
+            double[] error = new double[warstwy[warstwy.Count - 1].Length];
+            for (int i = 0; i < error.Length; i++)
+            {
+                error[i] = 0;
+            }
+            for (int i = 0; i < wejscia.GetLength(0); i++)
+            {
+                int wagi_miejsce = 0;
+                int bias_miejsce = 0;
+                propagacja(ref wagi_miejsce, ref bias_miejsce, wejscia, i);
+                propagacja_wsteczna(ref error, u, wejscia, wyjscie, ref wagi_miejsce, ref bias_miejsce, i);
+                propagacja(ref wagi_miejsce, ref bias_miejsce, wejscia, i);
+            }
+            for (int i = 0; i < error.Length; i++)
+            {
+                error1.Add(error[i]);
+            }
+        }
+        return error1;
+    }
+    public double sigmain(double x)
+    {
+        return (1 / (1 + Math.Exp(-x)));
+    }
+    public double pochodnasigmain(double x, double y)
+    {
+        return y * x * (1 - x);
+    }
+}
