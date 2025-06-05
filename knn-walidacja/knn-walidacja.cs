9c0813647c8e269d3f74696e07d379817a693d7f
pawson123xd <115405815+pawson123xd@users.noreply.github.com>
2025-06-05 23:12:13 +0200

    Update knn-walidacja.cs

--- knn-walidacja/knn-walidacja.cs
+++ knn-walidacja/knn-walidacja.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
 class dane
@@ -15,9 +16,10 @@ class dane
     }
     private void wczytywanie()
     {
+        /*
         try
         {
-            using (StreamReader reader = new StreamReader("C:\\Users\\Pawson\\source\\repos\\knn walidacja\\dane.txt"))
+            using (StreamReader reader = new StreamReader("C:\\Users\\Pawson\\source\\repos\\knn walidacja\\dane.txt")) // można wczytać z pliku ale trzeba podać scierzke do pliku 
             {
                 string line;
                 while ((line = reader.ReadLine()) != null)
@@ -39,6 +41,17 @@ class dane
         finally
         {
             Console.WriteLine("wczytywanie zakończone");
+        }
+        */
+        double[,] dane = { { 6.5, 3, 5.2, 2, 3 }, { 5.1, 2.5, 3, 1.1, 2 }, { 4.9, 3.1, 1.5, 0.1, 1 }, { 4.9, 3.1, 1.5, 0.1, 1 }, { 6.8, 3, 5.5, 2.1, 3 }, { 6.9, 3.1, 4.9, 1.5, 2 }, { 5.1, 3.3, 1.7, 0.5, 1 }, { 6, 2.9, 4.5, 1.5, 2 }, { 6.2, 2.8, 4.8, 1.8, 3 }, { 4.8, 3.1, 1.6, 0.2, 1 }, { 6.1, 2.8, 4, 1.3, 2 }, { 6.8, 3.2, 5.9, 2.3, 3 }, { 5.4, 3.4, 1.7, 0.2, 1 }, { 5, 2.3, 3.3, 1, 2 }, { 6.7, 2.5, 5.8, 1.8, 3 }, { 6.5, 2.8, 4.6, 1.5, 2 }, { 5, 3.4, 1.5, 0.2, 1 }, { 6.4, 2.7, 5.3, 1.9, 3 }, { 5.6, 3, 4.1, 1.3, 2 }, { 5.8, 2.7, 5.1, 1.9, 3 }, { 5.4, 3.9, 1.3, 0.4, 1 }, { 5.5, 2.4, 3.8, 1.1, 2 }, { 4.7, 3.2, 1.3, 0.2, 1 }, { 7.7, 3.8, 6.7, 2.2, 3 }, { 5.6, 2.8, 4.9, 2, 3 }, { 6.7, 3, 5, 1.7, 2 }, { 5.1, 3.5, 1.4, 0.2, 1 }, { 5.9, 3, 4.2, 1.5, 2 }, { 5, 3.2, 1.2, 0.2, 1 }, { 6.5, 3.2, 5.1, 2, 3 }, { 6.1, 2.6, 5.6, 1.4, 3 }, { 5.7, 4.4, 1.5, 0.4, 1 }, { 6, 2.7, 5.1, 1.6, 2 }, { 5.8, 4, 1.2, 0.2, 1 }, { 6.5, 3, 5.5, 1.8, 3 }, { 5.8, 2.7, 3.9, 1.2, 2 }, { 5, 3.5, 1.6, 0.6, 1 }, { 6, 2.2, 4, 1, 2 }, { 7.9, 3.8, 6.4, 2, 3 }, { 4.9, 3, 1.4, 0.2, 1 }, { 6.7, 3.1, 4.7, 1.5, 2 }, { 6.7, 3.3, 5.7, 2.5, 3 }, { 6.1, 2.9, 4.7, 1.4, 2 }, { 5, 3.5, 1.3, 0.3, 1 }, { 5.8, 2.8, 5.1, 2.4, 3 }, { 7.2, 3.6, 6.1, 2.5, 3 }, { 6.7, 3.1, 4.4, 1.4, 2 }, { 5.1, 3.5, 1.4, 0.3, 1 }, { 6.3, 3.3, 4.7, 1.6, 2 }, { 6.3, 3.4, 5.6, 2.4, 3 }, { 4.3, 3, 1.1, 0.1, 1 }, { 4.6, 3.1, 1.5, 0.2, 1 }, { 6.3, 3.3, 6, 2.5, 3 }, { 6.4, 2.9, 4.3, 1.3, 2 }, { 5.1, 3.8, 1.6, 0.2, 1 }, { 6.5, 3, 5.8, 2.2, 3 }, { 5.6, 2.5, 3.9, 1.1, 2 }, { 4.9, 3.1, 1.5, 0.1, 1 }, { 6.7, 3.3, 5.7, 2.1, 3 }, { 5, 2, 3.5, 1, 2 }, { 6.9, 3.1, 5.1, 2.3, 3 }, { 5.9, 3.2, 4.8, 1.8, 2 }, { 4.5, 2.3, 1.3, 0.3, 1 }, { 6.3, 2.5, 5, 1.9, 3 }, { 5.6, 2.7, 4.2, 1.3, 2 }, { 5.4, 3.9, 1.7, 0.4, 1 }, { 6.9, 3.1, 5.4, 2.1, 3 }, { 4.4, 3, 1.3, 0.2, 1 }, { 4.9, 2.4, 3.3, 1, 2 }, { 5, 3, 1.6, 0.2, 1 }, { 6.4, 2.8, 5.6, 2.2, 3 }, { 6.3, 2.3, 4.4, 1.3, 2 }, { 7.3, 2.9, 6.3, 1.8, 3 }, { 4.6, 3.2, 1.4, 0.2, 1 }, { 5.8, 2.7, 4.1, 1, 2 }, { 4.8, 3.4, 1.9, 0.2, 1 }, { 5.6, 2.9, 3.6, 1.3, 2 }, { 6.7, 3.1, 5.6, 2.4, 3 }, { 5.7, 2.5, 5, 2, 3 }, { 5.8, 2.6, 4, 1.2, 2 }, { 4.6, 3.4, 1.4, 0.3, 1 }, { 5.2, 2.7, 3.9, 1.4, 2 }, { 5.8, 2.7, 5.1, 1.9, 3 }, { 5, 3.6, 1.4, 0.2, 1 }, { 5.2, 3.5, 1.5, 0.2, 1 }, { 6.4, 3.2, 4.5, 1.5, 2 }, { 7.2, 3.2, 6, 1.8, 3 }, { 5.6, 3, 4.5, 1.5, 2 }, { 5.9, 3, 5.1, 1.8, 3 }, { 5.1, 3.7, 1.5, 0.4, 1 }, { 6.8, 2.8, 4.8, 1.4, 2 }, { 4.8, 3.4, 1.6, 0.2, 1 }, { 7.7, 3, 6.1, 2.3, 3 }, { 6.6, 3, 4.4, 1.4, 2 }, { 4.4, 2.9, 1.4, 0.2, 1 }, { 7.1, 3, 5.9, 2.1, 3 }, { 6.6, 2.9, 4.6, 1.3, 2 }, { 7.4, 2.8, 6.1, 1.9, 3 }, { 5.4, 3.7, 1.5, 0.2, 1 }, { 6.2, 3.4, 5.4, 2.3, 3 }, { 5.5, 2.3, 4, 1.3, 2 }, { 5.2, 4.1, 1.5, 0.1, 1 }, { 5, 3.4, 1.6, 0.4, 1 }, { 7, 3.2, 4.7, 1.4, 2 }, { 7.7, 2.8, 6.7, 2, 3 }, { 5.7, 3, 4.2, 1.2, 2 }, { 6.1, 3, 4.9, 1.8, 3 }, { 5.7, 3.8, 1.7, 0.3, 1 }, { 6, 3.4, 4.5, 1.6, 2 }, { 7.2, 3, 5.8, 1.6, 3 }, { 5.4, 3.4, 1.5, 0.4, 1 }, { 6.1, 3, 4.6, 1.4, 2 }, { 6.9, 3.2, 5.7, 2.3, 3 }, { 5, 3.3, 1.4, 0.2, 1 }, { 5.2, 3.4, 1.4, 0.2, 1 }, { 6.2, 2.9, 4.3, 1.3, 2 }, { 6.3, 2.9, 5.6, 1.8, 3 }, { 6.4, 3.2, 5.3, 2.3, 3 }, { 5.1, 3.8, 1.5, 0.3, 1 }, { 6.1, 2.8, 4.7, 1.2, 2 }, { 5.7, 2.9, 4.2, 1.3, 2 }, { 6, 2.2, 5, 1.5, 3 }, { 5.1, 3.8, 1.9, 0.4, 1 }, { 5.5, 4.2, 1.4, 0.2, 1 }, { 5.7, 2.8, 4.5, 1.3, 2 }, { 6.4, 2.8, 5.6, 2.1, 3 }, { 6.3, 2.5, 4.9, 1.5, 2 }, { 6, 3, 4.8, 1.8, 3 }, { 5.3, 3.7, 1.5, 0.2, 1 }, { 6.2, 2.2, 4.5, 1.5, 2 }, { 4.8, 3, 1.4, 0.3, 1 }, { 6.3, 2.7, 4.9, 1.8, 3 }, { 4.9, 2.5, 4.5, 1.7, 3 }, { 5.1, 3.4, 1.5, 0.2, 1 }, { 5.5, 2.4, 3.7, 1, 2 }, { 4.4, 3.2, 1.3, 0.2, 1 }, { 5.7, 2.6, 3.5, 1, 2 }, { 7.7, 2.6, 6.9, 2.3, 3 }, { 4.8, 3, 1.4, 0.1, 1 }, { 6.7, 3, 5.2, 2.3, 3 }, { 5.5, 2.5, 4, 1.3, 2 }, { 6.3, 2.8, 5.1, 1.5, 3 }, { 4.7, 3.2, 1.6, 0.2, 1 }, { 5.7, 2.8, 4.1, 1.3, 2 }, { 4.6, 3.6, 1, 0.2, 1 }, { 7.6, 3, 6.6, 2.1, 3 }, { 5.5, 2.6, 4.4, 1.2, 2 }, { 5.5, 3.5, 1.3, 0.2, 1 }, { 6.4, 3.1, 5.5, 1.8, 3 }, { 5.4, 3, 4.5, 1.5, 2 } };
+        for (int i = 0; i < dane.GetLength(0); i++)
+        {
+            double[] dane1 = new double[dane.GetLength(1)];
+            for(int j = 0; j < dane.GetLength(1); j++)
+            {
+                dane1[j]= dane[i, j];
+            }
+            list.Add(dane1);
         }
     }
     private void zmomalizować()
@@ -80,8 +93,6 @@ class dane
                 this.k = 1;
             }
             walidacja();
-            procenty();
-
         }
         private double manhattan(double[] a, double[] b)
         {
@@ -92,43 +103,92 @@ class dane
             }
             return wynik;
         }
+        private double euklidesowa(double[] a, double[] b)
+        {
+            double wynik = 0;
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                wynik += Math.Pow(a[i] - b[i],2);
+            }
+            return Math.Sqrt(wynik);
+        }
+        private double Czebyszewe(double[] a, double[] b)
+        {
+            double max = 0;
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                if (max < Math.Abs(a[i] - b[i]))
+                {
+                    max = Math.Abs(a[i] - b[i]);
+                }
+            }
+            return max;
+        }
+        private double logarytmem(double[] a, double[] b)
+        {
+            double wynik = 0;
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                wynik += Math.Log10(Math.Abs(a[i] - b[i]));
+            }
+            return wynik;
+        }
+        private double Minkowskiego(double[] a, double[] b)
+        {
+            double wynik = 0;
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                wynik+=Math.Pow(Math.Abs(a[i] - b[i]), 2);
+            }
+            return Math.Pow(wynik, (double)1 / 2);
+        }
         private void walidacja()
         {
-            metyka m = manhattan;
+            Console.WriteLine("eksperymentów 1 to manhattan");
+            Console.WriteLine("eksperymentów 2 to euklidesowa");
+            Console.WriteLine("eksperymentów 3 to Czebyszewe");
+            Console.WriteLine("eksperymentów 4 to logarytmem");
+            Console.WriteLine("eksperymentów 5 to Minkowskiego");
+            metyka[] m = {manhattan, euklidesowa, Czebyszewe , logarytmem,Minkowskiego};
             gropowanie g = grupowanie;
             duplicate s = duplicates;
-            for (int i = 0; i < list.Count; i++)
+            for (int k = 0; k < m.Length; k++)
             {
-                double[,] pamieć = new double[list.Count, 2];
-                for (int j = 0; j < list.Count; j++)
+                Console.WriteLine("eksperymentów {0}",k+1);
+                for (int i = 0; i < list.Count; i++)
                 {
-                    if (j == i)
+                    double[,] pamieć = new double[list.Count, 2];
+                    for (int j = 0; j < list.Count; j++)
                     {
-                        pamieć[j, 0] = 0;
-                        pamieć[j, 1] = list[j][list[j].Length - 1];
-                        continue;
-                    }
-                    pamieć[j, 0] = m(list[i], list[j]);
-                    pamieć[j, 1] = list[j][list[j].Length - 1];
-                }
-                bool Duplicates = s(g(pamieć));
-                if (Duplicates)
-                {
-                    List<double[]> a = g(pamieć);
-                    double min = a.Min(row => row[0]);
-                    for(int j = 0; j < a.Count; j++)
-                    {
-                        if (min == a[j][0])
+                        if (j == i)
                         {
-                            wynik.Add([min, a[j][1]]);
+                            pamieć[j, 0] = 0;
+                            pamieć[j, 1] = list[j][list[j].Length - 1];
+                            continue;
                         }
+                        pamieć[j, 0] = m[k](list[i], list[j]);
+                        pamieć[j, 1] = list[j][list[j].Length - 1];
                     }
+                    bool Duplicates = s(g(pamieć));
+                    if (Duplicates)
+                    {
+                        List<double[]> a = g(pamieć);
+                        double min = a.Min(row => row[0]);
+                        for (int j = 0; j < a.Count; j++)
+                        {
+                            if (min == a[j][0])
+                            {
+                                wynik.Add([min, a[j][1]]);
+                            }
+                        }
 
+                    }
+                    else
+                    {
+                        blad++;
+                    }
                 }
-                else
-                {
-                    blad++;
-                }
+                procenty();
             }
         }
         private List<double[]> grupowanie(double[,] a)
@@ -197,15 +257,18 @@ class dane
         }
         private void procenty()
         {
+
             Console.WriteLine("poprane wynik {0}%",(double)wynik.Count/list.Count*100);
             Console.WriteLine("niepoprane wynik {0}%",(double)blad/list.Count*100);
+            wynik.Clear();
+            blad = 0;
         }
     }
     class program
     {
         static void Main(string[] args)
         {
-            klasyfikacja dane = new klasyfikacja(1);
+            klasyfikacja dane = new klasyfikacja(3);
             Console.ReadKey();
         }
     }
